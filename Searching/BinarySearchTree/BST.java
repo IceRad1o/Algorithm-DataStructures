4bae0427feab1ac95c2b2f88c566bb88a94851d4
wjl <ra7more@outlook.com>
2017-03-08 20:42:42 +0800

    searching

--- Searching/BinarySearchTree/BST.java
+++ Searching/BinarySearchTree/BST.java
@@ -486,19 +486,7 @@ public class BST<Key extends Comparable<Key>, Value> {
      */
     public static void main(String[] args) { 
         BST<String, Integer> st = new BST<String, Integer>();
-        for (int i = 0; !StdIn.isEmpty(); i++) {
-            String key = StdIn.readString();
-            st.put(key, i);
-        }
-
-        for (String s : st.levelOrder())
-            StdOut.println(s + " " + st.get(s));
-
-        StdOut.println();
-
-        for (String s : st.keys())
-            StdOut.println(s + " " + st.get(s));
-    }
+      //Test Code
 }
 
 
